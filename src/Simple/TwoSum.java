9453f036c5f1cdab40cdd38402a60a5e475bca37
Saena <toastyhatesthesea@gmail.com>
2018-07-05 22:54:39 -0700

    Still Testing [2,4,3]               [5,6,4]

--- src/Simple/TwoSum.java
+++ src/Simple/TwoSum.java
@@ -92,8 +92,12 @@ public class TwoSum
 
             if (l2.next != null)
             {
-                current.next = new ListNode(0);
+                //current.next = new ListNode(0);
                 current = current.next;
+                if (current == null)
+                {
+                    current = new ListNode(0);
+                }
             }
 
             l2 = l2.next;
@@ -120,12 +124,15 @@ class SolutionTesters
     public static void main(String[] arsgs)
     {
         TwoSum rawrs = new TwoSum();
-        ListNode l1 = new ListNode(0);
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
         ListNode l1Head = l1;
 
 
-        ListNode l2 = new ListNode(7);
-        l2.next = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
         ListNode l2Head = l2;
 
         ListNode answer = rawrs.addTwoNumbers(l1Head, l2Head);
