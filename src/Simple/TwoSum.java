2787e897395d9a632e04dd50c82441e1053b5d3b
Saena <toastyhatesthesea@gmail.com>
2018-07-05 22:17:35 -0700

    Test Cases with Single Number and Two Numbers in the Second Node

--- src/Simple/TwoSum.java
+++ src/Simple/TwoSum.java
@@ -109,11 +109,12 @@ class SolutionTesters
     public static void main(String[] arsgs)
     {
         TwoSum rawrs = new TwoSum();
-        ListNode l1 = new ListNode(9);
+        ListNode l1 = new ListNode(0);
         ListNode l1Head = l1;
 
 
-        ListNode l2 = new ListNode(9);
+        ListNode l2 = new ListNode(7);
+        l2.next = new ListNode(3);
         ListNode l2Head = l2;
 
         ListNode answer = rawrs.addTwoNumbers(l1Head, l2Head);
