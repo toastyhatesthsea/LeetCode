c03e250032e4fac2f4c2c166448aaa6c5fe71cff
Saena <toastyhatesthesea@gmail.com>
2018-07-06 00:33:19 -0700

    Should be Finished

--- src/Simple/TwoSum.java
+++ src/Simple/TwoSum.java
@@ -106,9 +106,7 @@ class SolutionTesters
     public static void main(String[] arsgs)
     {
         TwoSum rawrs = new TwoSum();
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
+        ListNode l1 = new ListNode(9);
         ListNode l1Head = l1;
 
 
